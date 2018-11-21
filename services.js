5d46a2b82ec4abd58608e80a84230bd5be164ef4
Sean Higgins <shiggins@pagerduty.com>
2018-11-21 14:01:52 -0800

    Rename Demo Stations

--- services.js
+++ services.js
@@ -1,47 +1,47 @@
 var services = {
-        "01 Demo (Ken): Product Search": {
+        "01 Demo (Ken)": {
                 "CloudTrail": "d7c25cd46de04b41afd0412266b560da",
                 "CloudWatch": "704feb51158e47359ebf872a3ad43aee",
                 "GuardDuty": "7d481e115c294f9cbf77c2796137607d",
                 "New Relic": "f56a6e39342c42f4bf3dc05015f3123a",
                 "PHD": "f377979711fa41b593f57284fb6ebce4"
         },
-        "02 Demo (Eric): Cloud Storage": {
+        "02 Demo (Eric)": {
                 "CloudTrail": "55e8ab68e5f046849c5a5729431a6d77",
                 "CloudWatch": "61b6d2a5944b453292291dcc34d180e1",
                 "GuardDuty": "f4c066879faf42e5aba43de9ee9d97ae",
                 "New Relic": "63864c39d71d4f3b9b40ba016d8549fc",
                 "PHD": "301367190c5f4fd19198fe3d30c01cc4"
         },
-        "03 Demo (Masado): Authentication": {
+        "03 Demo (Masado)": {
                 "CloudTrail": "a5793607eb7d4a78a67a365cfa8ba4c1",
                 "CloudWatch": "b2e5f82381b94abd8cc15bdc7519be54",
                 "GuardDuty": "e300f6f3f3064464810362446b0d6414",
                 "New Relic": "5977fc1453a44149b2b68d463960c297",
                 "PHD": "1e2e28c35ec6433e9070bf2478a6699b"
         },
-        "04 Demo (Edward): Payment Processing": {
+        "04 Demo (Edward)": {
                 "CloudTrail": "1937a580428d4e2fa32dca1b3169d052",
                 "CloudWatch": "67066c29b9234a77aa08ae4ad2402c3c",
                 "GuardDuty": "104c29322da74b2d86af634f9294428b",
                 "New Relic": "d784391ffb9c45c991dec0db8d17cd6c",
                 "PHD": "2454e628fd85466a8ad82654d215eef1"
         },
-        "05 Demo (John): Shopping Cart Backend": {
+        "05 Demo (John)": {
                 "CloudTrail": "427ece0b82854ec1ba36f89f7fd1a52a",
                 "CloudWatch": "2a63716a97024493b592952753b60773",
                 "GuardDuty": "55f233731f424cb1bb7292578f854092",
                 "New Relic": "334c8d1b409948059bd313e1390747f6",
                 "PHD": "4c2fbc028e5043cca507431927f4efd0"
         },
-        "06 Demo (Cheryl): Online Catalog": {
+        "06 Demo (Cheryl)": {
                 "CloudTrail": "675721bbfba54c7780ce37849c082bd5",
                 "CloudWatch": "71e65a93e1d74f61b9de5f74c29b1a59",
                 "GuardDuty": "ea0c802e91b24d15b460771bdf210f79",
                 "New Relic": "09cd84f4ac634710a8f1cdbe4318617c",
                 "PHD": "f6d63d9cb6604b12883518008f99330b"
         },
-        "07 Demo (Mary): Coupon Delivery": {
+        "07 Demo (Mary)": {
                 "CloudTrail": "ed0f44a8fb1245f6852569a13de650c8",
                 "CloudWatch": "ed0f44a8fb1245f6852569a13de650c8",
                 "GuardDuty": "a76adb20e99f461e9d9458405bacc47b",
